33b0e8ce97d62006b1d8a330237eedacf39a9753
kwon <wingseter@gmail.com>
2025-02-04 20:55:14 +0900

    hash-4

---
+++ C++/Programers/algorithmKit/Hash/4.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 0;
+    if(clothes.size() == 0) return 0;
+    
+    unordered_map<string, int> collect;
+    
+    for (auto cloth : clothes) {
+        string categories = cloth[1];
+        collect[categories] += 1;
+    }
+    
+    answer = 1;
+    for (auto& value : collect){
+        answer *= (value.second + 1);
+    }
+    answer -= 1;
+    
+    return answer;
+}
