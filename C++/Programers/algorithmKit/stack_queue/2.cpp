a90ea4d470d45d2e877272c9b149160082a918b7
kwon <wingseter@gmail.com>
2025-02-05 21:27:19 +0900

    stack_hash - 2

---
+++ C++/Programers/algorithmKit/stack_queue/2.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+    
+    for(int i = 0; i < progresses.size(); i++) {
+        q.push(ceil((100.0 - progresses[i]) / speeds[i]));
+    }
+    
+   int biggest = -987654321;
+    
+    while(!q.empty())
+    {
+        int front = q.front();
+        if (front > biggest) {
+            biggest = front;
+            answer.push_back(1);
+        }
+        else {
+            answer.back() += 1;
+        }
+        q.pop();
+    }
+    
+    return answer;
+}
