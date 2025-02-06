628b6e9539f399c09e770fa734a742ca53a0e891
kwon <wingseter@gmail.com>
2025-02-06 21:17:41 +0900

    heap-1

---
+++ C++/Programers/algorithmKit/Heap/1.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    
+    for(int scov : scoville) {
+        pq.push(scov);
+    }
+    
+
+    int first, second, new_food;
+    while(pq.size() > 1) {
+       if (pq.top() >= K) {
+            return answer;
+        }
+        
+        first = pq.top();
+        pq.pop();
+        second = pq.top();
+        pq.pop();
+        
+        new_food = first + second * 2;
+        
+        answer += 1;
+        pq.push(new_food);
+        
+    }
+    
+    if (pq.top() < K) answer = -1;
+
+    return answer;
+}
